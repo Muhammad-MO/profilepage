e3c166ef24460760a8306d8ceab29eb583278665
Muhammad <muhdmahmood@gmail.com>
2024-06-26 01:33:07 +0800

    push all code to profile git

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//leaflet js
+
+var map = L.map('map').setView([1.290270, 103.851959], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
